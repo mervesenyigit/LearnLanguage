3f22937188fb6961fad03f4be64a2bb4700c40bd
MERVE ŞENYİĞİT <merve.senyigit@outlook.com>
2023-06-21 15:47:58 +0300

    LearnLanguage_FirstCommit

---
+++ LearnLanguage/ClientApp/src/components/Level.js
@@ -0,0 +1,29 @@
+﻿import React, { useState, useEffect } from 'react';
+
+const LevelItems = () => {
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    useEffect(() => {
+        fetch(`ìtem/${dataType}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+    })
+    return (
+
+        <main>
+            {
+                (items != null) ? items.map((items) => <h3>{items.tit}</h3>)
+            }
+          <div>
+
+
+            </div>
+
+        </main>
+    )
+}
---
+++ LearnLanguage/Models/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LearnLanguage.Models
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Voice> Voices { get; set; }
+        public DbSet<Level> Levels { get; set; }
+
+    }
+}
